e59371bf3fee279132582f67f75c99554f57a8e0
Sreeju Ramachandran <sreejutr@gmail.com>
2018-01-27 17:29:01 -0500

    new release 2 class

---
+++ src/org/ssoft/training/git/ReleaseTwoClass.java
@@ -0,0 +1,7 @@
+package org.ssoft.training.git;
+
+public class ReleaseTwoClass {
+	public static void main(String[] args) {
+		System.out.println("This is the new class added in release Two");
+	}
+}
